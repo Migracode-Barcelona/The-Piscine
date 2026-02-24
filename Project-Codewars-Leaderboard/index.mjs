b8e2888679a488ba05922086fb8d42f17f58d5bf
jannahpitogo <pitogojannah@outlook.com>
2026-02-24 21:59:40 +0100

    I have fixed the function for adding the users in the input form and displaying the users appended under it.
    Remove first all the functions and exports from input-user to make sure that the functionality is working

--- Project-Codewars-Leaderboard/index.mjs
+++ Project-Codewars-Leaderboard/index.mjs
@@ -1,10 +1,75 @@
-import { displayInputForm } from "./input-user.mjs";
 
-export function makeFetchRequest() {
-  return fetch("https://example.com/test");
+let getUser = [];
+let user = "";
+
+function setup() {
+  displayInputForm();
+  buttonUser();
+
 }
 
- 
-displayInputForm();
+function displayInputForm() {
+    const inputContainer = document.getElementById("input-form"); //div 
+
+    const userInput = document.createElement("input");  //input text
+    userInput.type = "text";
+    userInput.id = "user-input";
+
+    const addUserButton = document.createElement("button"); //adduser button
+    addUserButton.id = "adding-user"
+    addUserButton.textContent = "Add User"
+
+    const showUsers = document.createElement("p"); //p (paragraph for user)
+    showUsers.id = "showing-users"; 
+    // showUsers.innerHTML = "";
+    // showUsers.textContent = "Hello World";
+    // showUsers.hidden = true; 
+
+    const userContainer = document.createElement("div"); //div p container
+    userContainer.id = "users-container";
+    
+    inputContainer.append(userInput, addUserButton);   
+
+    userContainer.appendChild(showUsers);
+    const body = document.querySelector("body"); 
+    body.appendChild(userContainer);
+}
+
+
+function displayUser() {
+    const inputElement = document.getElementById("user-input"); //input
+    getUser.push(inputElement.value);
+    // console.log(getUser);
+
+    //displaying  
+    const userContainer = document.createElement("div"); //div p container
+    userContainer.id = "users-container";
+    const showUsers = document.getElementById("showing-users"); //p 
+    // showUsers.hidden = false;
+    
+    // for (let element of getUser) {
+    //     user += `${element} `;
+    //     // console.log("hello");
+    //     return user;
+    // }
+    // console.log(user);
+
+    showUsers.textContent = `${getUser} `;
+    // showUsers.textContent = user;
+    
+}
+
+
+function buttonUser(){
+  const addUserButton = document.getElementById("adding-user");
+  addUserButton.addEventListener("click", displayUser);
+}
+
+
+window.onload = setup;
+
+
+
+
 
 
