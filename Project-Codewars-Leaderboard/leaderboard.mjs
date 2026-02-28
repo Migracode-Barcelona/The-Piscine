780dd6e1ac253d74aa1f8f3523c711ac56707be3
jannahpitogo <pitogojannah@outlook.com>
2026-02-28 12:44:09 +0100

    cleaned the code, remove from functions and fixed some bugs from forwardings

--- Project-Codewars-Leaderboard/leaderboard.mjs
+++ Project-Codewars-Leaderboard/leaderboard.mjs
@@ -3,88 +3,78 @@ import * as userData from "./components/get-users-input.mjs";
 let tableData = [];
 let moreUser = [];
 
-function setup() {
-  leaderboardButtons();
-  addNavCategory();
-  addTable();
-  backButtonForwarding();
-  addUserLeaderboard();
-  addMoreUsers();
-}
+//BACK-BUTTON AND ADD MORE USERS BUTTON (LEADERBOARD PAGE)
+const buttonsContainer = document.getElementById("button-container");
 
-function leaderboardButtons() {
-  const buttonsContainer = document.getElementById("button-container");
-  const backButton = document.createElement("a");
-  backButton.id = "back-button";
-  backButton.textContent = "Back to Arena";
-  const addAnotherUserButton = document.createElement("button");
-  addAnotherUserButton.type = "button";
-  addAnotherUserButton.classList = "btn btn-primary";
-  //   addAnotherUserButton.setAttribute("data-bs-toggle", "modal");
-  //   addAnotherUserButton.setAttribute("data-bs-target", "#myModal");
-  addAnotherUserButton.id = "add-another_user";
-  addAnotherUserButton.textContent = "Add Another User";
+const backButton = document.createElement("a");
+backButton.id = "back-button";
+backButton.textContent = "Back to Arena";
 
-  buttonsContainer.append(backButton, addAnotherUserButton);
-}
+const addAnotherUserButton = document.createElement("button");
+addAnotherUserButton.type = "button";
+addAnotherUserButton.classList = "btn btn-primary";
+addAnotherUserButton.id = "add-another_user";
+addAnotherUserButton.textContent = "Add Another User";
 
-function addNavCategory() {
-  const overall = document.createElement("button");
-  overall.id = "overall";
-  overall.textContent = "Overall";
-  const completedKatas = document.createElement("button");
-  completedKatas.id = "completed-katas";
-  completedKatas.textContent = "Completed Katas";
-  const authoredKatas = document.createElement("button");
-  authoredKatas.id = "authored-katas";
-  authoredKatas.textContent = "Authored Katas";
-  const ranks = document.createElement("button");
-  ranks.id = "ranks";
-  ranks.textContent = "Ranks";
-  const categoryContainer = document.getElementById("category-nav");
+buttonsContainer.append(backButton, addAnotherUserButton);
+//
 
-  categoryContainer.append(overall, completedKatas, authoredKatas, ranks);
-}
+//CATEGORY NAVIGATION
+const overall = document.createElement("button");
+overall.id = "overall";
+overall.textContent = "Overall";
 
-//Displaying the table
-function addTable() {
-  const table = document.createElement("table");
-  table.id = "table-leaderboards";
-  const column = document.createElement("tr");
-  const tableHead0 = document.createElement("th");
-  tableHead0.textContent = "POSITION";
-  const tableHead1 = document.createElement("th");
-  tableHead1.textContent = "USER";
-  const tableHead2 = document.createElement("th");
-  tableHead2.textContent = "CLAN";
-  const tableHead3 = document.createElement("th");
-  tableHead3.textContent = "HONOR";
-  const tableContainer = document.getElementById("table-container");
+const completedKatas = document.createElement("button");
+completedKatas.id = "completed-katas";
+completedKatas.textContent = "Completed Katas";
 
-  column.append(tableHead0, tableHead1, tableHead2, tableHead3);
-  table.appendChild(column);
-  tableContainer.appendChild(table);
+const authoredKatas = document.createElement("button");
+authoredKatas.id = "authored-katas";
+authoredKatas.textContent = "Authored Katas";
 
-  for (let i = 0; i < userData.getUser.length; i++) {}
-}
+const ranks = document.createElement("button");
+ranks.id = "ranks";
+ranks.textContent = "Ranks";
 
-function backButtonForwarding() {
-  const backButton = document.getElementById("back-button");
-  backButton.addEventListener("click", () => {
-    backButton.href = "index.html";
-  });
-}
+const categoryContainer = document.getElementById("category-nav");
+categoryContainer.append(overall, completedKatas, authoredKatas, ranks);
+//
 
-//MODAL  FOR ADD USER
+//DISPLAYING THE TABLE
+const table = document.createElement("table");
+table.id = "table-leaderboards";
+
+const column = document.createElement("tr");
+
+const tableHead0 = document.createElement("th");
+tableHead0.textContent = "POSITION";
+
+const tableHead1 = document.createElement("th");
+tableHead1.textContent = "USER";
+
+const tableHead2 = document.createElement("th");
+tableHead2.textContent = "CLAN";
+
+const tableHead3 = document.createElement("th");
+tableHead3.textContent = "HONOR";
+
+const tableContainer = document.getElementById("table-container");
+
+column.append(tableHead0, tableHead1, tableHead2, tableHead3);
+table.appendChild(column);
+tableContainer.appendChild(table);
+
+for (let i = 0; i < userData.getUser.length; i++) {}
+
+//
+
+//MODAL  FOR ADD MORE USER
 function modalAddUser() {
-  //   if (document.getElementById("myModal")) {
-  //     document.getElementById("myModal").remove();
-  //   }
-
   const modalContainer = document.createElement("div");
   modalContainer.classList = "modal fade";
   modalContainer.id = "myModal";
   modalContainer.tabIndex = -1;
+  //   modalContainer.removeAttribute("aria-hidden");
   modalContainer.innerHTML = `<div class="modal-dialog">
       <div class="modal-content">
 
@@ -111,23 +101,27 @@ function modalAddUser() {
   modal.show();
 }
 
-function addUserLeaderboard() {
-  const addUserButton = document.getElementById("add-another_user");
-  addUserButton.addEventListener("click", () => {
-    modalAddUser();
-  });
-}
+//BUTTON FOR ADDING MORE USERS (EVENTLISTENER)
+const addUserButton = document.getElementById("add-another_user");
+addUserButton.addEventListener("click", () => {
+  modalAddUser();
+});
+// }
 
-//FUNCTION FOR SUBMIT BUTTON FOR ADD MORE USERS
-function addMoreUsers() {
-  const addUser = document.getElementById("submit-more-users");
-  addUser.addEventListener("click", () => {
-    userData.gettingInput("more-users", moreUser);
-    // console.log(moreUser);
-  });
-}
+//BACK-BUTTON(EVENTLISTENER)
+backButton.addEventListener("click", () => {
+  backButton.href = "index.html";
+});
+//
+
+// FUNCTION FOR SUBMIT BUTTON FOR ADD MORE USERS
+// function addMoreUsers() {
+//   const addUser = document.getElementById("submit-more-users");
+//   addUser.addEventListener("click", () => {
+//     userData.gettingInput("more-users", moreUser);
+//     // console.log(moreUser);
+//   });
+// }
 
 //CONTINUE
 //GETTING USERS FROM SUBMIT BUTTON IN ADDING MORE USERS AND PUSHING THEM INTO THE ARRAY
-
-window.onload = setup;
