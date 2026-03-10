d0d89e419ef2b1b5c53204a41335479b0e04b1a8
jannahpitogo <pitogojannah@outlook.com>
2026-03-10 12:23:36 +0100

    added a functionality for duplicated username and unique usernames

--- Project-Codewars-Leaderboard/leaderboard.mjs
+++ Project-Codewars-Leaderboard/leaderboard.mjs
@@ -2,6 +2,7 @@ import * as userData from "./components/get-users-input.mjs";
 import * as display from "./components/display.mjs";
 
 let userArray = JSON.parse(sessionStorage.getItem("users")) ?? []; //RETRIEVE USERS FROM INDEX
+let fetchedUsers = JSON.parse(sessionStorage.getItem("fetchedUsers")) ?? [];
 
 //BACK-BUTTON AND ADD MORE USERS BUTTON (LEADERBOARD PAGE)
 display.addMoreButton();
@@ -9,6 +10,7 @@ display.addMoreButton();
 //CATEGORY NAVIGATION
 display.categoryDisplay();
 display.tableDisplay();
+
 display.modalAddingUser();
 
 //BUTTON FOR ADDING MORE USERS (EVENTLISTENER)
@@ -20,7 +22,7 @@ addUserButton.addEventListener("click", () => {
 
 //FUNCTION FOR SUBMIT BUTTON FOR ADD MORE USERS
 const addMoreUser = document.getElementById("submit-more-users");
-addMoreUser.addEventListener("click", () => {
+addMoreUser.addEventListener("click", async () => {
   const newUsers = userData.gettingInput(
     "more-users",
     userArray,
@@ -30,8 +32,21 @@ addMoreUser.addEventListener("click", () => {
   );
 
   if (newUsers.length > 0) {
+    const newlyFetchedUsers = await userData.fetchUsers(newUsers);
+
+    const mergedUsers = [...fetchedUsers, ...newlyFetchedUsers];
+    const uniqueByUsername = [];
+    const seen = new Set();
+    for (const user of mergedUsers) {
+      if (!user?.username || seen.has(user.username)) continue;
+      seen.add(user.username);
+      uniqueByUsername.push(user);
+    }
+
+    fetchedUsers = uniqueByUsername;
     sessionStorage.setItem("users", JSON.stringify(userArray));
+    sessionStorage.setItem("fetchedUsers", JSON.stringify(fetchedUsers));
+    
     console.log("[Local] Added users:", newUsers);
   }
 });
-
